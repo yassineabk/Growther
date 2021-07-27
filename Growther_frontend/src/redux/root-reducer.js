9a44d1bd5e9a4975a94a4a2b66f94cae3eb5cf08
med <MEDBAAMRANI0@GMAIL.COM>
2021-07-27 14:20:01 +0100

    fix-errors

--- Growther_frontend/src/redux/root-reducer.js
+++ Growther_frontend/src/redux/root-reducer.js
@@ -2,7 +2,6 @@ import {combineReducers} from 'redux'
 import contestReducer from './contest/contest-reducer'
 import {registrationReducer } from './registration/registration.reducers'
 import {loginReducer} from './login/login.reducer'
-import { authReducer } from './auth/auth.reducers'
 import ContestCard from './contest-card/contest-card-reducer'
 import EditReducer from './contest-edit/contest-edit-reducer'
 
