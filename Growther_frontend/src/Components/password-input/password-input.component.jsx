669459eae2863b8764c625d4ed1d513ac766e132
med <MEDBAAMRANI0@GMAIL.COM>
2021-08-06 14:24:43 +0100

    show pass

--- Growther_frontend/src/Components/password-input/password-input.component.jsx
+++ Growther_frontend/src/Components/password-input/password-input.component.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 
-const PasswordInput=({handleChange,handleBlur,label,isError,value,message,placeholder})=>(
 
+const PasswordInput=({handleChange,handleBlur,label,isError,value,message,placeholder})=>{
+    
+    const [showPassword, setshowPassword] = useState(true);
+    const handleClickShowPassword = () => {
+        console.log("cliiic")
+        setshowPassword(!showPassword);
+      };
+      
+     
+    return(
+    
     <div className="field authInput">
         <label className="label">{label}</label>
         <div className="control has-icons-left has-icons-right input_container">
-            <input className={`${isError ? 'is-danger' :'form-input-color'} input`} value={value} onBlur={handleBlur} onChange={handleChange} type="password" placeholder={placeholder}/>
+            <input style={{zIndex:1}} className={`${isError ? 'is-danger' :'form-input-color'} input`} 
+                    value={value} 
+                    onBlur={handleBlur} 
+                    onChange={handleChange} 
+                    type={showPassword ? "text" : "password"}
+                    placeholder={placeholder}/>
             <span className="icon is-small is-left">
                 <i className="fas fa-lock"></i>
+            </span>
+            <span style={{zIndex:5,cursor:"pointer"}} onClick={()=>console.log("mmmmmm")} id="eye" className="icon is-small is-right">
+                <i  className={`${showPassword ? 'fas fa-eye-slash' :'fas fa-eye'}`}></i>
             </span>
         </div>
         <p className={`${isError ? 'help is-danger' :'help is-sucess'}`}>{message}</p>
@@ -15,5 +34,5 @@ const PasswordInput=({handleChange,handleBlur,label,isError,value,message,placeh
 
 
 
-)
+)}
 export default PasswordInput;
